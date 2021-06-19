aec4a3515b11cfc49363022e2682b45d0265fd45
Stefan Stammberger <some.fusion@gmail.com>
2021-06-19 16:20:47 +0200

    feat: add async bitcoin RPC func; refactor config
    refactor: generalize the way the .env file is read and accessed

--- app/utils.py
+++ app/utils.py
@@ -1,11 +1,32 @@
+from app.routers import bitcoin
+from types import coroutine
+import aiohttp
 import requests
 import json
 from decouple import config
 
-BITCOIND_IP = config("bitcoind_ip")
-BITCOIND_PORT = config("bitcoind_port")
-BITCOIND_USER = config("bitcoind_user")
-BITCOIND_PW = config("bitcoind_pw")
+
+class BitcoinConfig:
+    def __init__(self) -> None:
+        self.network = config("network")
+
+        if(self.network == "testnet"):
+            self.ip = config("bitcoind_ip_testnet")
+            self.rpc_port = config("bitcoind_port_testnet")
+            self.zmq_port = config("bitcoind_port_zmq_testnet")
+        else:
+            self.ip = config("bitcoind_ip_mainnet")
+            self.rpc_port = config("bitcoind_port_mainnet")
+            self.zmq_port = config("bitcoind_port_zmq_mainnet")
+
+        self.rpc_url = f"http://{self.ip}:{self.rpc_port}"
+        self.zmq_url = f"tcp://{self.ip}:{self.zmq_port}"
+
+        self.username = config("bitcoind_user")
+        self.pw = config("bitcoind_pw")
+
+
+bitcoin_config = BitcoinConfig()
 
 
 def bitcoin_rpc(method: str, params: list = []) -> requests.Response:
@@ -20,9 +41,19 @@ def bitcoin_rpc(method: str, params: list = []) -> requests.Response:
     params : list, optional
         Any parameters to include with the call
     """
-    url = f"http://{BITCOIND_IP}:{BITCOIND_PORT}"
-    auth = (BITCOIND_USER, BITCOIND_PW)
+    auth = (bitcoin_config.username, bitcoin_config.pw)
     headers = {"Content-type": "text/plain"}
     data = '{"jsonrpc": "2.0", "method": "' + \
         method + '", "id":"0", "params":' + json.dumps(params) + '}'
-    return requests.post(url, auth=auth, headers=headers, data=data)
+    return requests.post(bitcoin_config.rpc_url, auth=auth, headers=headers, data=data)
+
+
+async def bitcoin_rpc_async(method: str, params: list = []) -> coroutine:
+    auth = aiohttp.BasicAuth(bitcoin_config.username, bitcoin_config.pw)
+    headers = {"Content-type": "text/plain"}
+    data = '{"jsonrpc": "2.0", "method": "' + \
+        method + '", "id":"0", "params":' + json.dumps(params) + '}'
+
+    async with aiohttp.ClientSession(auth=auth, headers=headers) as session:
+        async with session.post(bitcoin_config.rpc_url, data=data) as resp:
+            return await resp.json()
