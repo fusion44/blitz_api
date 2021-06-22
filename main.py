f03785474891522ed6d8c4cbcc59a975cc8d8dcf
Stefan Stammberger <some.fusion@gmail.com>
2021-06-22 21:07:15 +0200

    feat: implement basic SSE using Redis

--- main.py
+++ main.py
@@ -1,20 +1,51 @@
-from fastapi import FastAPI
+from aioredis import Channel, Redis
+from fastapi import FastAPI, Depends
+from fastapi_plugins import (get_config, depends_redis, registered_configuration,
+                             redis_plugin, RedisSettings)
+from sse_starlette.sse import EventSourceResponse
 from app.routers import apps, bitcoin, system
-
 
 # start server with "uvicorn main:app --reload"
 
+
+@registered_configuration
+class AppSettings(RedisSettings):
+    api_name: str = str(__name__)
+
+
 app = FastAPI()
+config = get_config()
 
 app.include_router(apps.router)
 app.include_router(bitcoin.router)
 app.include_router(system.router)
 
 
-# @app - path operation decorator
-# .get - path operation
-# '/' - the path
+@app.on_event('startup')
+async def on_startup() -> None:
+    await redis_plugin.init_app(app, config=config)
+    await redis_plugin.init()
+
+
+@app.on_event('shutdown')
+async def on_shutdown() -> None:
+    await redis_plugin.terminate()
+
+
 @app.get('/')
 def index():
     # path operation function
     return {'data': '123'}
+
+
+@app.get("/sse/subscribe")
+async def stream(channel: str = "default", redis: Redis = Depends(depends_redis)):
+    return EventSourceResponse(subscribe(channel, redis))
+
+
+async def subscribe(channel: str, redis: Redis):
+    (sub,) = await redis.subscribe(channel=Channel(channel, False))
+
+    while await sub.wait_message():
+        data = await sub.get(encoding='utf-8')
+        yield {"event": "event_id", "data": data}
