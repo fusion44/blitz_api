2ab56ca7748e6f84609433909218ff8103013165
Christoph Stenglein <christoph@cstenglein.com>
2021-10-04 17:36:56 +0200

    only allow localhst:3000 since other ports are not needed

--- app/main.py
+++ app/main.py
@@ -45,9 +45,14 @@ app = VersionedFastAPI(
     enable_latest=True,
 )
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
