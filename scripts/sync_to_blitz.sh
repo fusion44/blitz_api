b8c5a2eab3144d999551e42ba05f83120d527f6e
rootzoll <christian@geektank.de>
2022-05-02 17:45:00 +0200

    fix & document code sync to dev blitz

--- scripts/sync_to_blitz.sh
+++ scripts/sync_to_blitz.sh
@@ -42,6 +42,18 @@ fi
 local=.
 remote=admin@$localIP:/root/blitz_api
 
+# clean pycache just in case the code was compiled locally before
+rm -r ./app/__pycache__
+rm -r ./app/repositories/ln_impl/protos/__pycache__
+rm -r ./app/repositories/ln_impl/__pycache__
+rm -r ./app/external/fastapi_versioning/__pycache__
+rm -r ./app/external/sse_starlette/__pycache__
+rm -r ./app/external/__pycache__
+rm -r ./app/models/__pycache__
+rm -r ./app/repositories/__pycache__
+rm -r ./app/routers/__pycache__
+rm -r ./app/auth/__pycache__
+
 # Needs sshpass installed
 echo "# syncing local code to: ${remote}"
 sshpass -p "$passwordA" rsync -rvz -e "ssh -p ${sshPort}" $local $remote
