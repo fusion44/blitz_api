f592ba0b315ffb61684cf2b5ffa50205a177bb0a
fusion44 <some.fusion@gmail.com>
2022-09-11 10:08:50 +0200

    feat: define AppsBase as abstract

--- app/repositories/apps_impl/apps_base.py
+++ app/repositories/apps_impl/apps_base.py
@@ -1,15 +1,23 @@
+from abc import abstractmethod
+
+
 class AppsBase:
+    @abstractmethod
     async def get_app_status_single(self, app_id: str):
         raise NotImplementedError()
 
+    @abstractmethod
     async def get_app_status(self):
         raise NotImplementedError()
 
+    @abstractmethod
     async def get_app_status_sub(self):
         raise NotImplementedError()
 
+    @abstractmethod
     async def install_app_sub(self, app_id: str):
         raise NotImplementedError()
 
+    @abstractmethod
     async def uninstall_app_sub(self, app_id: str, delete_data: bool):
         raise NotImplementedError()
