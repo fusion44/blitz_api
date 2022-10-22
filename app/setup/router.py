36e2d525b7b0b0ceffe45f01178338be839a3a02
fusion44 <some.fusion@gmail.com>
2022-10-22 12:08:05 +0200

    fix: setup endpoint not showing on RaspiBlitz

--- app/setup/router.py
+++ app/setup/router.py
@@ -5,5 +5,5 @@ from app.system.models import APIPlatform
 router = None
 
 _PLATFORM = config("platform", default=APIPlatform.RASPIBLITZ)
-if _PLATFORM != APIPlatform.RASPIBLITZ:
+if _PLATFORM == APIPlatform.RASPIBLITZ:
     from app.setup.impl.raspiblitz.router import router
