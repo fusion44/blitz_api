f6389c0541a161ef60c1bef6717d4d2b85316d64
fusion44 <some.fusion@gmail.com>
2022-12-11 19:23:22 +0100

    feat: add support for more lightning nodes

--- docker/regtest/entrypoint.sh
+++ docker/regtest/entrypoint.sh
@@ -3,8 +3,16 @@
 
 if [ "$LN_BACKEND" = "cln-1" ]; then
   cp /code/.env.cln1 /code/.env
-else
+elif [ "$LN_BACKEND" = "cln-2" ]; then
+  cp /code/.env.cln2 /code/.env
+elif [ "$LN_BACKEND" = "lnd-1" ]; then
   cp /code/.env.lnd1 /code/.env
+elif [ "$LN_BACKEND" = "lnd-2" ]; then
+  cp /code/.env.lnd2 /code/.env
+elif [ "$LN_BACKEND" = "lnd-3" ]; then
+  cp /code/.env.lnd3 /code/.env
+else
+  echo "Unknown LN_BACKEND: $LN_BACKEND"
 fi
 
 # wait until the nodes are ready
