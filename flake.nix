149bd30b15fe00a87294bc2ad253fca536fe9064
fusion44 <some.fusion@gmail.com>
2024-12-16 15:20:46 +0100

    chore: update deps and flake file

--- flake.nix
+++ flake.nix
@@ -1,35 +1,63 @@
 {
-  description = "Application packaged using poetry2nix";
+  description = "A management backend for the RaspiBlitz project written in Python / FastAPI";
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     poetry2nix = {
-      url = "github:fusion44/poetry2nix";
+      url = "github:nix-community/poetry2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
-        pkgs = nixpkgs.legacyPackages.${system};
-        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
-      in
-      {
-        packages = {
-          myapp = mkPoetryApplication { projectDir = self; };
-          default = self.packages.${system}.myapp;
-        };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    poetry2nix,
+  }: let
+    name = "blitz-api";
 
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ self.packages.${system}.myapp ];
-          packages = with pkgs; [
-            poetry
-            pyright
-            sshpass
-          ];
+    systems = flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      inherit (poetry2nix.lib.mkPoetry2Nix {inherit pkgs;}) mkPoetryApplication overrides mkPoetryEnv;
+      poetryDev = mkPoetryEnv {
+        projectDir = ./.;
+        preferWheels = true;
+        overrides = overrides.withDefaults (final: prev: {
+          ruff = prev.ruff.override {
+            preferWheel = true;
+          };
+        });
+      };
+    in {
+      packages = {
+        default = self.packages.${system}.${name};
+        ${name} = mkPoetryApplication {
+          projectDir = ./.;
+          meta.mainProgram = name;
         };
-      });
+        poetryDev = mkPoetryEnv {
+          projectDir = ./.;
+          preferWheels = true;
+        };
+      };
+
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          poetry
+          poetryDev
+          pyright
+          alejandra
+        ];
+      };
+    });
+
+    overlays.overlays = {
+      default = final: prev: {
+        ${name} = self.packages.${prev.stdenv.hostPlatform.system}.${name};
+      };
+    };
+  in
+    systems // overlays;
 }
